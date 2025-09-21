25bafc2e1d3b174a560269087d0082e7c9e1245a
Jagriti130 <pandeyjagriti209@gmail.com>
2025-09-22 01:10:27 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x):
+        left,right=1,x
+        while left<=right:
+            mid=(left+right)//2
+            if mid*mid==x:
+                return mid
+            if mid*mid>x:
+                right=mid-1
+            else:
+                left=mid+1
+        return right
+
+        
+        
